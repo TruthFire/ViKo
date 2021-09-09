b4797b1865264a38368787bc86d53a3fe1e656ef
TruthFire <truth.fire@yandex.ru>
2021-09-10 02:41:44 +0300

    5

--- OOP/PU2/PU2/Form1.cs
+++ OOP/PU2/PU2/Form1.cs
@@ -17,7 +17,7 @@ namespace PU2
             InitializeComponent();
         }
 
-        private string isNullOrSpace(string s)
+        private string IsNullOrSpace(string s)
         {
             if (s == null || s.Trim() == string.Empty)
             {
@@ -27,12 +27,22 @@ namespace PU2
 
         }
 
+        private DateTime IsDateValid(DateTime d)
+        {
+            DateTime curr = DateTime.Today;
+            if (!(d.Day <= curr.Day && d.Month < curr.Month && d.Year < curr.Year))
+            {
+                throw new ArgumentException("Gimimo data negali buti didesni, nei esama");
+            }
+            return d;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             Person p;
             try
             {
-                p = new Person(isNullOrSpace(this.textBox1.Text), isNullOrSpace(this.textBox2.Text), DateTime.Parse(this.textBox3.Text));
+                p = new Person(IsNullOrSpace(this.textBox1.Text), IsNullOrSpace(this.textBox2.Text), IsDateValid(DateTime.Parse(this.textBox3.Text)));
                 MessageBox.Show(p.PrintInfo(), "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception exc)
