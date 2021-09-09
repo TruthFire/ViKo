8a473663eb902c6274d61678c69b34364c2cca43
TruthFire <truth.fire@yandex.ru>
2021-09-10 02:52:43 +0300

    Git fix

---
+++ OP/PU2/PU2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PU2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string IsNullOrSpace(string s)
+        {
+            if (s == null || s.Trim() == string.Empty)
+            {
+                throw new ArgumentException("Ivedimas ne gali buti tuscias arba lygus null");
+            }
+            return s;
+
+        }
+
+        private DateTime IsDateValid(DateTime d)
+        {
+            DateTime curr = DateTime.Today;
+            if (!(d.Day <= curr.Day && d.Month < curr.Month && d.Year < curr.Year))
+            {
+                throw new ArgumentException("Gimimo data negali buti didesni, nei esama");
+            }
+            return d;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Person p;
+            try
+            {
+                p = new Person(IsNullOrSpace(this.textBox1.Text), IsNullOrSpace(this.textBox2.Text), IsDateValid(DateTime.Parse(this.textBox3.Text)));
+                MessageBox.Show(p.PrintInfo(), "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception exc)
+            {
+                this.textBox1.Text = ""; this.textBox2.Text = ""; this.textBox3.Text = "";
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+    }
+}
---
+++ OP/PU2/PU2/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PU2
+{
+   public class Person
+{
+
+	private string Name, Surename;
+	private DateTime Dob; 
+
+
+	public Person(string name, string surename, DateTime dob)
+	{
+		this.Name = name;
+		this.Surename = surename;
+		this.Dob = dob;
+	}
+
+	public string GetAge()
+    {
+		return DateTime.Now.AddYears(-(Dob.Year + Convert.ToInt32(IsBdBeen()))).ToString("yy");
+	}
+
+	public int DaysToBd() // kiek dienu liko iki gd
+    {
+		DateTime curr = DateTime.Now;
+		int year = curr.Year + Convert.ToInt32(!IsBdBeen()); //
+		double days = (new DateTime(year, Dob.Month, Dob.Day) - curr).TotalDays;
+			return (int)days;
+	}
+	
+	private bool IsBdBeen() // ar buvo gimtadienis simet?
+    {
+		bool rez = false;
+		DateTime curr = DateTime.Now;
+			if (curr.Month < Dob.Month || curr.Month == Dob.Month && curr.Day < Dob.Day) //jei buvo = grazinam true(1), else false (0)
+				rez = true;
+
+			return rez;
+    }
+
+	public string PrintInfo()
+    {
+		string s = "Vardas: " + Name + "\nPavarde: " + Surename + "\nAmžius: " + GetAge() + "\nDienus iki gimtadienio: " + DaysToBd();
+            return s;
+    }
+}
+}
